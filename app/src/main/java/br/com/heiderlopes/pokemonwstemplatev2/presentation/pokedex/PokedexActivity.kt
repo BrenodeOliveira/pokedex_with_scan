6c08352e9e7f0936b51b983a131839a2f26484b2
Breno Rocha <brenorocha947@gmail.com>
2022-12-08 22:32:20 -0300

    Fixing bugs

--- app/src/main/java/br/com/heiderlopes/pokemonwstemplatev2/presentation/pokedex/PokedexActivity.kt
+++ app/src/main/java/br/com/heiderlopes/pokemonwstemplatev2/presentation/pokedex/PokedexActivity.kt
@@ -24,7 +24,7 @@ class PokedexActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pokedex)
+        setContentView(binding.root)
 
         viewModel.getPokemon(pokemonNumber)
 
